eeb5b794daa941a504068bd5e192ad260b949db5
AntoineBlondon <antoine@blondon.fr>
2023-12-02 18:46:33 +0100

    playing with timers

--- native_functions.py
+++ native_functions.py
@@ -432,10 +432,13 @@ def native_create_app(native_context):
 
         async def on_mount(self):
             # Dock all widgets in the widgets list
+            logging.debug("Linking bindings")
             for binding in self.BINDINGS:
                 self.bind(keys=binding.key, action="do_nothing('" + binding.key + "')", description=binding.description)
+            logging.debug("Docking widgets")
             for widget in self.widgets:
                 await self.view.dock(widget)
+            logging.debug("Done")
 
 
         def on_key(self, event: events.Key):
